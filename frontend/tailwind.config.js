76e6883a48ca8edead53bfdaf58d837ab71cf674
YogaSky <140642473+YogaSky@users.noreply.github.com>
2023-08-29 18:01:21 +0800

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = { 
-	content: [ "./src/**/*.{js,ts,jsx,tsx}" ], 
-	theme: { 
-		extend: {}, 
-	}, 
-	plugins: [], 
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+		screens: {
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+			'md': '768px',
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+		},
+	},
+	plugins: [],
 }
