f6ebf89de733931a22c3d6d8d1f2839f2a01ac0e
YogaSky <140642473+YogaSky@users.noreply.github.com>
2023-08-29 17:37:29 +0800

    Update page.tsx

--- frontend/src/app/[lang]/blog/[category]/page.tsx
+++ frontend/src/app/[lang]/blog/[category]/page.tsx
@@ -1,73 +1,53 @@
+import PageHeader from '@/app/[lang]/components/PageHeader';
 import { fetchAPI } from '@/app/[lang]/utils/fetch-api';
-import Post from '@/app/[lang]/views/post';
-import type { Metadata } from 'next';
+import BlogList from '@/app/[lang]/views/blog-list';
 
-async function getPostBySlug(slug: string) {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/articles`;
-    const urlParamsObject = {
-        filters: { slug },
-        populate: {
-            cover: { fields: ['url'] },
-            authorsBio: { populate: '*' },
-            category: { fields: ['name'] },
-            blocks: { populate: '*' },
-        },
-    };
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await fetchAPI(path, urlParamsObject, options);
-    return response;
+async function fetchPostsByCategory(filter: string) {
+    try {
+        const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+        const path = `/articles`;
+        const urlParamsObject = {
+            sort: { createdAt: 'desc' },
+            filters: {
+                category: {
+                    slug: filter,
+                },
+            },
+            populate: {
+                cover: { fields: ['url'] },
+                category: {
+                    populate: '*',
+                },
+                authorsBio: {
+                    populate: '*',
+                },
+            },
+        };
+        const options = { headers: { Authorization: `Bearer ${token}` } };
+        const responseData = await fetchAPI(path, urlParamsObject, options);
+        return responseData;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-async function getMetaData(slug: string) {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/articles`;
-    const urlParamsObject = {
-        filters: { slug },
-        populate: { seo: { populate: '*' } },
-    };
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await fetchAPI(path, urlParamsObject, options);
-    return response.data;
-}
+export default async function CategoryRoute({ params }: { params: { category: string } }) {
+    const filter = params.category;
+    const { data } = await fetchPostsByCategory(filter);
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-    const meta = await getMetaData(params.slug);
-    const metadata = meta[0].attributes.seo;
+    //TODO: CREATE A COMPONENT FOR THIS
+    if (data.length === 0) return <div>Not Posts In this category</div>;
 
-    return {
-        title: metadata.metaTitle,
-        description: metadata.metaDescription,
-    };
-}
+    const { name, description } = data[0]?.attributes.category.data.attributes;
 
-export default async function PostRoute({ params }: { params: { slug: string } }) {
-    const { slug } = params;
-    const data = await getPostBySlug(slug);
-    if (data.data.length === 0) return <h2>no post found</h2>;
-    return <Post data={data.data[0]} />;
+    return (
+        <div>
+            <PageHeader heading={name} text={description} />
+            <BlogList data={data} />
+        </div>
+    );
 }
 
 export async function generateStaticParams() {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/articles`;
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const articleResponse = await fetchAPI(
-        path,
-        {
-            populate: ['category'],
-        },
-        options
-    );
-
-    return articleResponse.data.map(
-        (article: {
-            attributes: {
-                slug: string;
-                category: {
-                    slug: string;
-                };
-            };
-        }) => ({ slug: article.attributes.slug, category: article.attributes.slug })
-    );
+    return [];
 }
